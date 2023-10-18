0b252e749ef29c5611f0f6881a2b6df2387860df
Ben5903 <60116122+Ben5903@users.noreply.github.com>
2023-10-18 20:48:38 +0100

    Problem 2
    Completed problem 2, including a .flatmap and .reduce  to show the word frequency map for all post body contents.

--- Lab3/problem-2/scripts.js
+++ Lab3/problem-2/scripts.js
@@ -22,6 +22,23 @@ fetchData("posts")
             .map(post => post.title);
         console.log("Post Titles with More Than Six Words:");
         console.log(longPostTitles);
+
+        // Problem 2: Show a word frequency map for all post body contents
+        const allPostBodies = posts.flatMap(post => post.body); // Use flatMap to flatten the body contents
+        const wordFrequencyMap = WordFrequencyMap(allPostBodies);
+        console.log("Word Frequency Map for Post Bodies:");
+        console.log(wordFrequencyMap);
     })
     .catch(error => console.error(error));
 
+// Function to generate a word frequency map from text data
+function WordFrequencyMap(textArray) {
+    const words = textArray
+        .join(" ")
+        .toLowerCase()
+        .match(/\w+/g); // Extract words using regex
+    return words.reduce((map, word) => {
+        map[word] = (map[word] || 0) + 1;
+        return map;
+    }, {});
+}
